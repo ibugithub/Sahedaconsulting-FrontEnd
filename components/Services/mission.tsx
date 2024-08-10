2dba9c9dac45697d2980c27af0296ac76b52410e
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2024-08-10 22:45:38 +0600

    make the mission page mobile responsive

--- components/Services/mission.tsx
+++ components/Services/mission.tsx
@@ -10,31 +10,31 @@ export const Mission = () => {
           <div className="relative h-64 w-full lg:w-1/2 mb-8 lg:mb-0 lg:mr-8 rounded-lg overflow-hidden shadow-lg">
             <Image src={mission} layout="fill" objectFit="cover" alt="Our mission" />
           </div>
-          <div className="text-center lg:text-left lg:w-1/2">
+          <div className="text-center lg:text-left lg:w-1/2 space-y-6">
             <h2 className="text-lg font-semibold text-gray-700">OUR MISSION</h2>
-            <h1 className="mt-2 text-4xl font-extrabold text-gray-900">Turn Ideas Into Reality</h1>
-            <p className="mt-4 text-md text-gray-800">
+            <h1 className="text-4xl font-extrabold text-gray-900">Turn Ideas Into Reality</h1>
+            <p className="text-md text-gray-800">
               The mission of our engineering consulting service company is to deliver comprehensive engineering, management, and IT solutions with precision and innovation. We aim to empower clients, optimize processes, and drive technological excellence to meet the evolving challenges of today’s dynamic business landscape.
             </p>
-            <div className="mt-12">
-              <div className="flex items-center lg:items-start gap-5">
-                <div className="flex  justify-start">
-                  <FaCheckCircle className="text-blue-600 text-2xl mr-1" />
-                  <div className="flex flex-col text-gray-800">
+            <div className="space-y-4">
+              <div className="flex flex-col sm:flex-row items-center justify-center lg:justify-start gap-5">
+                <div className="flex items-center">
+                  <FaCheckCircle className="text-blue-600 text-2xl mr-2" />
+                  <div className="flex flex-col text-gray-800 text-center sm:text-left">
                     <span className="text-lg font-medium">Engineering</span>
                     <span className="text-lg">80%</span>
                   </div>
                 </div>
-                <div className="flex  justify-start">
-                  <FaCheckCircle className="text-green-600 text-2xl mr-1" />
-                  <div className="flex flex-col text-gray-800">
+                <div className="flex items-center">
+                  <FaCheckCircle className="text-green-600 text-2xl mr-2" />
+                  <div className="flex flex-col text-gray-800 text-center sm:text-left">
                     <span className="text-lg font-medium">Management</span>
                     <span className="text-lg">90%</span>
                   </div>
                 </div>
-                <div className="flex  justify-start">
-                  <FaCheckCircle className="text-red-600 text-2xl mr-1" />
-                  <div className="flex flex-col text-gray-800">
+                <div className="flex items-center">
+                  <FaCheckCircle className="text-red-600 text-2xl mr-2" />
+                  <div className="flex flex-col text-gray-800 text-center sm:text-left">
                     <span className="text-lg font-medium">IT</span>
                     <span className="text-lg">87%</span>
                   </div>
