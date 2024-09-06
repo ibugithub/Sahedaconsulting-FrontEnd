babc8f3708f136be89e9a0de71be025c9e7b15d4
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2024-09-06 20:07:08 +0600

    wrap around with sunpense at app/verifyEmail/page.tsx

--- app/verifyEmail/page.tsx
+++ app/verifyEmail/page.tsx
@@ -1,12 +1,21 @@
-"use client"
+"use client";
 import { VerifyEmail } from "@/components/Profile/verifyEmail";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+
 const Page = () => {
   const searchParams = useSearchParams();
   const token = searchParams.get('token');
-  if(!token){
-    return <div>invalid token</div>
+  if (!token) {
+    return <div>invalid token</div>;
   }
-  return <VerifyEmail token={token} />
-}
-export default Page;
+  return <VerifyEmail token={token} />;
+};
+
+const SuspenseWrapper = () => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <Page />
+  </Suspense>
+);
+
+export default SuspenseWrapper;
